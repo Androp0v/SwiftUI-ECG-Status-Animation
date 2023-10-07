3eab92b32648e7cd5ac6dc8a2b9d2170d9bc9946
Raúl Montón Pinillos <raul_m_p@me.com>
2023-10-08 00:30:50 +0200

    Initial Commit

---
+++ SwiftUI ECG Status Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI ECG Status Animation
+//
+//  Created by Raúl Montón Pinillos on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI ECG Status Animation/SwiftUI_ECG_Status_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ECG_Status_AnimationApp.swift
+//  SwiftUI ECG Status Animation
+//
+//  Created by Raúl Montón Pinillos on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ECG_Status_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
